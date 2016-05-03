29e7a7c13a5bd54b498d43f371755f561c13cd78
Simon Coope <Simon Coope>
2016-05-03 20:38:56 +0100

    Added command line example

---
+++ CommandLineTools/Program.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+
+namespace DNX.Demo
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            WriteLine("Application Running");
+            Read();        
+        }
+    }
+}
